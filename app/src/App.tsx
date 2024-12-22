f892af7db6fe8b299075f8b4eb043a1de1c13902
Ki Wook Kim <wook.kim@rwth-aachen.de>
2024-12-22 16:41:06 +0100

    api

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -146,7 +146,7 @@ const App: React.FC = () => {
                     console.log("Conversation Context:", conversationContext);
     
                     // Send main API request for response
-                    const response = await fetch("/api/v1/slm/TheraSol/chat", {
+                    const response = await fetch("https://api.assisterr.ai/api/v1/slm/TheraSol/chat/", {
                         method: "POST",
                         headers: {
                             "X-Api-Key": apiKey,
@@ -164,7 +164,7 @@ const App: React.FC = () => {
                     setConversationHistory((prevHistory) => [...prevHistory, responseData.message]);
     
                     // emotion Detection 
-                    const emotionResponse = await fetch("/api/v1/slm/motionundle/chat", {
+                    const emotionResponse = await fetch("https://api.assisterr.ai/api/v1/slm/motionundle/chat", {
                         method: "POST",
                         headers: {
                             "X-Api-Key": emotionAPIKey,
